1cddb1e58fbd366806a020c7231512bd50c5cdd5
Hung D Nguyen <gumstash@gmail.com>
2018-10-20 03:11:46 -0700

    Added Raycast Vision
    Still need to draw enemy fov

---
+++ FallJam2018/Assets/Hung/Scripts/FieldOfView.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour {
+    public float viewRadius;
+    [Range(0,360)]
+    public float viewAngle;
+
+    public LayerMask playerMask;
+    public LayerMask obstacleMask;
+
+    public bool playerSeen = false;
+    public Transform playerPos;
+
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
+
+    void FindVisibleTargets()
+    {
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, playerMask);
+        playerSeen = false;
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if(Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float dstToTarget = Vector3.Distance(transform.position, target.position);
+                if(!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask))
+                {
+                    playerSeen = true;
+                    playerPos = target;
+                }
+            }
+   
+        }
+    }
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("FindTargetsWithDelay", 0.2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
