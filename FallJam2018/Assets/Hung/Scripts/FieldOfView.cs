4346c4312cabf4ac13a718789079bc52f4c875ef
Hung D Nguyen <hungdacnguyen99@gmail.com>
2018-10-20 15:18:30 -0700

    Player Enemy Interaction
    shooting and dying.

--- FallJam2018/Assets/Hung/Scripts/FieldOfView.cs
+++ FallJam2018/Assets/Hung/Scripts/FieldOfView.cs
@@ -74,12 +74,20 @@ public class FieldOfView : MonoBehaviour {
 
     public IEnumerator AttackCo()
     {
+<<<<<<< HEAD
         this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
         yield return new WaitForSeconds(aimTime);
         BulletController newBullet = Instantiate(bullet, firePoint.transform.position, firePoint.transform.rotation) as BulletController;
         newBullet.speed = bulletSpeed;
         this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
         this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX;
+=======
+        this.GetComponent<Rigidbody>().isKinematic = true;
+        yield return new WaitForSeconds(aimTime);
+        BulletController newBullet = Instantiate(bullet, GetComponent<Rigidbody>().transform.position, GetComponent<Rigidbody>().transform.rotation) as BulletController;
+        newBullet.speed = bulletSpeed;
+        this.GetComponent<Rigidbody>().isKinematic = false;
+>>>>>>> Player Enemy Interaction
     }
 
     void DrawFieldOfView()
