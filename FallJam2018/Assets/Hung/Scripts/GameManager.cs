fe79c2c77cdf493ea272f795c2f206ca0442bfe1
Hung D Nguyen <hungdacnguyen99@gmail.com>
2018-10-20 15:18:30 -0700

    Player Enemy Interaction
    shooting and dying.

---
+++ FallJam2018/Assets/Hung/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private Vector3 playerStartPoint;
+    public Movement player;
+    public GameObject deathParticle;
+
+	// Use this for initialization
+	void Start () {
+        playerStartPoint = player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RestartGame()
+    {
+        StartCoroutine("RestartGameCo");
+    }
+
+    public IEnumerator RestartGameCo()
+    {
+        Instantiate(deathParticle, player.gameObject.transform.position, player.gameObject.transform.rotation);
+        player.gameObject.SetActive(false);
+        yield return new WaitForSeconds(4);
+        player.transform.position = playerStartPoint;
+        player.gameObject.SetActive(true);
+    }
+}
