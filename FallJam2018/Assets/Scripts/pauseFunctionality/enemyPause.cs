63de2b79881e29424b2552a68ad59b0110322d31
seantsukim <seantsukim@gmail.com>
2018-10-20 18:20:11 -0700

    started on pause scripts
    initialized c# scripts

---
+++ FallJam2018/Assets/Scripts/pauseFunctionality/enemyPause.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyPause : MonoBehaviour {
+
+	public GameObject[] enemies; //array of enemies
+	public int enemyNum; //number of enemies in array
+
+	/*
+	Stops movement of Enemy GameObjects
+	disabling and re-enabling scripts */
+	public void pauseEnemies()
+	{
+		for(int i = 0; i < enemyNum; i++)
+		{
+		}
+	}
+}
---
+++ FallJam2018/Assets/Scripts/pauseFunctionality/playerPause.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerPause : MonoBehaviour {
+
+	public GameObject player; //player object to manipulate
+
+	/*
+	stops the player, diabling and re-enabling its movement script
+	when paused. */
+	public void pausePlayer()
+	{
+		
+	}
+}
