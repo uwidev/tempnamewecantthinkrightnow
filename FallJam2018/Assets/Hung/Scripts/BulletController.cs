e80fc2f0e47688e929ea3e5be298fd4c7ab54d63
seantsukim <seantsukim@gmail.com>
2018-10-20 20:47:34 -0700

    Reset

--- FallJam2018/Assets/Hung/Scripts/BulletController.cs
+++ FallJam2018/Assets/Hung/Scripts/BulletController.cs
@@ -14,6 +14,40 @@ public class BulletController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Player Enemy Interaction
+=======
+>>>>>>> 08639421c7fbe6345df2aba31c2178bd978b9739
+=======
+>>>>>>> 08639421c7fbe6345df2aba31c2178bd978b9739
         Destroy(gameObject, 3);
     }
+=======
+	}
+>>>>>>> Player Enemy Interaction
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+        Destroy(gameObject, 3);
+    }
+>>>>>>> Shooting and DYing
+=======
+>>>>>>> Player Enemy Interaction
+=======
+=======
+>>>>>>> 08639421c7fbe6345df2aba31c2178bd978b9739
+=======
+        Destroy(gameObject, 3);
+    }
+>>>>>>> 3a212a7dce3d6edde9bdd79514aedafd6620b427
+<<<<<<< HEAD
+>>>>>>> 08639421c7fbe6345df2aba31c2178bd978b9739
+=======
+>>>>>>> 08639421c7fbe6345df2aba31c2178bd978b9739
 }
